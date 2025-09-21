68cd834a8a97aa5123b636be0b3b148f9a530f0b
misoj8 <jxnnyA8@proton.me>
2025-09-21 01:15:04 +0000

    Update simple2-buttonUI.cpp
    super basic UI for a command line based program using c++ basics
    
    3 default options:
    - start
    - end
    - invalid input

--- simple2-buttonUI.cpp
+++ simple2-buttonUI.cpp
@@ -1,29 +1,27 @@
-#include <iostream>                                                                                                                                                   
-                                                                                              
-                                                                                              
-int main()                                                                                    
-{                                                                                             
-                                                                                              
-    char buttInput;                                                                           
-    while(true)                                                                               
-    {                                                                                         
-        std::cout << "press x to begin\n" << "press z to quit\n";                             
-        std::cin  >> buttInput;                                                               
-        if (!std::cin) {return 0;} // infinite loop (eof error) prevention                                                     
-                                                                                              
-        if(buttInput == 'x')                                                                  
-        {                                                                                     
-                //initiate program                                                            
-                                                                                              
-        }                                                                                     
-        else if(buttInput == 'z')                                                             
-        {                                                                                     
-            std::cout << "Goodbye\n";                                                         
-            return 0;                                                                         
-        }                                                                                     
-        else                                                                                  
-        {                                                                                     
-            std::cout << "invalid option >:(\n";                                              
-        }                                                                                     
-    }                                                                                         
-}          
+#include <iostream>
+
+int main()
+{
+    char buttInput;
+    while(true)
+    {
+        std::cout << "press x to begin\n" << "press z to quit\n";
+        std::cin  >> buttInput;
+        if (!std::cin) {return 0;} // eof error prevention
+
+        if(buttInput == 'x')
+        {
+           //initiate program
+           //     here
+        }
+        else if(buttInput == 'z')
+        {
+            std::cout << "Goodbye\n";
+            return 0;
+        }
+        else
+        {
+            std::cout << "invalid option >:(\n";
+        }
+    }
+}
